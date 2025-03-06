98b903815657f2c8a3079122b811a59482de6803
UlianaPostevka06 <sudravijdmitro@gmail.com>
2025-03-06 19:56:56 +0200

    Update add.js

--- cafe/js/add.js
+++ cafe/js/add.js
@@ -1,13 +1,57 @@
-let favourite_btn = document.querySelector(".product__favorite")
-let addtocart_btn = document.querySelector(".plus__btn")
+const plusButtons = document.querySelectorAll(".plus__btn");
+const favouriteButtons = document.querySelectorAll(".product__favorite");
 
+function getCookie(name) {
+    let match = document.cookie.match(new RegExp('(^| )'+ name + '=([^;]+)'));
+    return match ? match[2] : null;
+}   
 
+function setCookie(name, value) {
+    document.cookie = `${name}=${value}; path=/; max-age=31536000`;
+}
 
-addtocart_btn.addEventListener("click", function() {
-    addtocart_btn.innerHTML = "fa-solid fa-check"
-    addtocart_btn.style.background = "rgb(0, 141, 89)"
-})
+document.addEventListener("DOMContentLoaded", function() {
+    plusButtons.forEach((btn, index) => {
+        const icon = btn.querySelector("i");
+        const storedState = getCookie(`plusBtn${index}`);
 
-favourite_btn.addEventListener("click", function() {
-    addtocart_btn.innerHTML = "fa-solid fa-heart"
-})
+        if (storedState === "checked") {
+            icon.classList.replace("fa-plus", "fa-check");
+            btn.style.backgroundColor = "#008D59";
+        }
+
+        btn.addEventListener("click", function(){
+            if (icon.classList.contains("fa-plus")) {
+                icon.classList.replace("fa-plus", "fa-check");
+                btn.style.backgroundColor = "#008D59";
+                setCookie(`plusBtn&{index}`, "checked");
+            } else {
+                icon.classList.replace("fa-check", "fa-plus");
+                btn.style.backgroundColor = "black";
+                setCookie(`plusBtn&{index}`, "unchecked");
+            }
+        });
+    });
+
+    favouriteButtons.forEach((btn, index) => {
+        const icon = btn.querySelector("i");
+        const storedState = getCookie(`plusBtn${index}`);
+
+        if (storedState === "liked") {
+            icon.classList.replace("fa-regular", "fa-solid");
+            icon.style.color = "#B71C1C";
+        }
+
+        btn.addEventListener("click", function () {
+            if(icon.classList.contains("fa-regular")) {
+                icon.classList.replace("fa-regular", "fa-solid");
+                icon.style.color = "#B71C1C";
+                setCookie(`favBtn&{index}`, "liked");
+            } else {
+                icon.classList.replace("fa-solid", "fa-regular");
+                icon.style.color = "black";
+                setCookie(`favBtn&{index}`, "unliked");
+            }
+        });
+    });
+});
